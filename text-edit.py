eff30a0ed00587e887c224da708e74ed01a21d81
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-08 15:57:50 +0200

    Update text-edit.py

--- text-edit.py
+++ text-edit.py
@@ -4,9 +4,6 @@ import tkinter.filedialog as fd
 import tkinter.messagebox as mb
 from PIL import Image, ImageTk
 import os
-import urllib
-image = urllib.URLopener()
-image.retrieve("http://www.gunnerkrigg.com//comics/00000001.jpg","00000001.jpg")
 root = Tk()
 root.title("Text-edit")
 root.geometry('800x500')
